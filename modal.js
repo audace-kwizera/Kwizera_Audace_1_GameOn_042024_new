2bb95c3ed9b8e6a7fd5b8a0fd7a480eb919848b0
Audace Kwizera <josueaudace@gmail.com>
2024-04-04 10:44:47 +0200

    init

---
+++ modal.js
@@ -0,0 +1,104 @@
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+
+/// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+
+  // Catch Data
+
+  // ====================== First Name //  One element
+  // Select element
+  let first = document.getElementById('first');
+  // Take value
+  first.addEventListener('input', () => {
+    //Display value
+    console.log(first.value);
+  });
+
+  // ====================== Last Name // One element
+   //Select element
+  let last = document.getElementById('last');
+  // Take value
+  last.addEventListener('input', () => {
+    //Display value
+    console.log(last.value);
+  });
+
+  // ====================== Email // One element
+   //Select element
+  let email = document.getElementById('email');
+  // Take value
+  email.addEventListener('input', () => {
+    //Display value
+    console.log(email.value);
+  });
+
+  // ====================== Bithday
+  //Select element
+  let birthdateInput = document.getElementById('birthdate');
+  birthdateInput.addEventListener('input', () => {
+    // Take value
+    let birthdate = birthdateInput.value;
+    //Display value
+    console.log(birthdate);
+  });
+
+  // ====================== Participation Number
+   //Select element
+   let participationInput = document.getElementById('quantity');
+   // Take value live
+   participationInput.addEventListener('input', () => {
+     let participation = participationInput.value;
+     //Display value
+     console.log(participation);
+   });
+
+  // ====================== City
+  //Select element
+  let cityInputs = document.querySelectorAll('input[name="location"]');
+   // Take value live
+  cityInputs.forEach(input => {
+    input.addEventListener('change', () => {
+      //Display value
+      if (input.checked) {
+        console.log(input.value);
+      }
+    });
+  });
+
+  // ====================== Condition
+  //Select element
+  let termsCheckbox = document.getElementById('checkbox1');
+  // Take value live
+  termsCheckbox.addEventListener('change', () => {
+    //Display value
+    console.log(termsCheckbox.checked);
+  });
+
+  // ====================== Newsletter
+  //Select element
+  let newsletterCheckbox = document.getElementById('checkbox2');
+  // Take value live
+  newsletterCheckbox.addEventListener('change', () => {
+    //Display value
+    console.log(newsletterCheckbox.checked);
+  });
+}
